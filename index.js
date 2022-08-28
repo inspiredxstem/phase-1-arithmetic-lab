ebeb861126dfcc3bbc2ab9bcbfc192a3135e28ac
Steven Rosario <strosariogarcia@gmail.com>
2022-08-28 18:55:20 -0400

    commit tree a96b4b7ddb2ef819709580e4a33d823f75bc3869

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-
 // Write your code here
+let num1 = 2, num2 = 31, num3 = 19, num4 = 5;
+
+const multiply = num1 * num2;
+const random = Math.floor(Math.random()*10);
+const mod = num3 % num4;
+const max = Math.max(20);
